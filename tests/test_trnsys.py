82cbc97ea14edc8ee520468217b223c06133193e
John Dyreby <john@isentropic.dev>
2023-06-22 11:17:36 -0500

    Create Simulation class (#2)

---
+++ tests/test_trnsys.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+import pytest
+
+
+from trnpy.trnsys.simulation import Simulation
+from trnpy.trnsys.lib import TrnsysLib, TrnsysDirectories, track_lib_path
+from trnpy.exceptions import DuplicateLibraryError, TrnsysStepForwardError
+
+
+class MockTrnsysLib(TrnsysLib):
+    def __init__(self):
+        self.is_at_end = False
+
+    def setDirectories(self, dirs: TrnsysDirectories) -> int:
+        return 0
+
+    def loadInputFile(self, input_file: Path) -> int:
+        return 0
+
+    def stepForward(self, steps: int) -> (bool, int):
+        if self.is_at_end:
+            return (False, 1)
+        return (True, 0)
+
+
+def test_track_lib_path_raises_duplicate_error_on_same_path():
+    track_lib_path(Path("a_lib_file"))
+    track_lib_path(Path("another_lib_file"))  # different file path, so ok
+    with pytest.raises(DuplicateLibraryError):
+        track_lib_path(Path("a_lib_file"))
+
+
+def test_stepping_after_final_time_raises_step_forward_error():
+    sim = Simulation(
+        MockTrnsysLib(),
+        TrnsysDirectories("", "", ""),
+        Path(""),
+    )
+
+    sim.lib.is_at_end = False
+    sim.step_forward(1)  # not at end of simulation, so ok
+
+    sim.lib.is_at_end = True
+    with pytest.raises(TrnsysStepForwardError):
+        sim.step_forward(1)
