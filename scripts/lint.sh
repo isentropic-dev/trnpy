14b35454fd9992a0226f43af15166a74b3e30788
Greg Troszak <gdtroszak@gmail.com>
2023-09-14 12:00:37 -0400

    Oops.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -euxo pipefail
 
-mypy -p src/trnpy -p tests
+mypy src tests
 black --target-version py311 --check .
 isort --profile black --check --diff src/trnpy/ tests/
 flake8 src/trnpy/ tests/
